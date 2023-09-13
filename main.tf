411bc4cc7a99c8bd6e69753a51490fe8d86ec364
thameem008 <140794340+thameem008@users.noreply.github.com>
2023-09-13 19:23:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,1 @@
-resource "local_file" "new_pet" {
-  filename = var.filename
-  content  = var.file_content
-}
-variable "filename" {
-  default = "new_pets.txt"
-}
-variable "file_content" {
-  default = "Our new pet is a cute kitten!"
-}
+
